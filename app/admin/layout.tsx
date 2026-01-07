f6608c0cfa3ccf2b42c2c3d14b7c707d498506ec
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-07 21:00:15 +0530

    completed

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -6,12 +6,9 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <div className="flex min-h-screen">
-        {/* Sidebar - Fixed on the left */}
         <div className="w-64 bg-gray-800 text-white fixed h-full">
           <Sidebar />
         </div>
-
-        {/* Main Content Area */}
         <div className="flex-1 flex flex-col ml-64 p-6">
           <ToastContainer theme="dark" />
           {children}
