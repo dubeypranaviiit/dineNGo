c7e6131cf03478364e557650feab12dc1bf73337
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-01-07 21:27:47 +0530

    completed

--- database/models/reservation.modal.ts
+++ database/models/reservation.modal.ts
@@ -1,68 +1,8 @@
-// import mongoose, { Schema, Document } from "mongoose";
 
-// export interface IReservation extends Document {
-//   name: string;
-//   email: string;
-//   date: Date;
-//   phone: number;
-//   time: string;
-//   guests:number,
-//   isConfirmed:boolean,
-//   approvedBy:string,
-//   approvedAt:Date
-// }
-// const ReservationSchema: Schema = new Schema(
-//   {
-//     name: {
-//          type: String,
-//          required: true
-//          },
-//     email: {
-//          type: String, 
-//          required: true
-//          },
-//     phone: { 
-//         type: Number, 
-//         required: true
-//      },
-//      date: {
-//           type: Date,
-//           required: true 
-//           },
-//     time: { 
-//         type: String, 
-//         required: true 
-//     },
- 
-//     guests: {
-//          type: Number,
-//          min: 1,
-//          max: 30,
-//          default: 1,
-//          },
-//      isConfirmed:{
-//         type:Boolean,
-//         default:false
-//      },
-//      approvedBy: { 
-//         type: String, 
-//         default: null 
-//     },
-//     approvedAt: { 
-//         type: Date,
-//          default: null 
-//         }    
-//   },
-//   { timestamps: true }
-// );
-
-
-//  const Reservation =mongoose.models.Reservation || mongoose.model<IReservation>("Reservation", ReservationSchema);
-//  export default Reservation;
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IReservation extends Document {
-  user?: mongoose.Types.ObjectId; // optional link to User
+  user?: mongoose.Types.ObjectId; 
   name: string;
   email: string;
   phone: string;
@@ -100,8 +40,6 @@ const ReservationSchema = new Schema<IReservation>(
   },
   { timestamps: true }
 );
-
-// Compound index to quickly query availability for a timeslot
 ReservationSchema.index({ date: 1, time: 1 });
 
 export default mongoose.models.Reservation ||
