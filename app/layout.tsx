c51ffc193b449d1485ddd5bfdc19c353cff8314e
Pranav Abhimanyu <146646970+dubeypranaviiit@users.noreply.github.com>
2026-03-05 23:21:03 +0530

    Import Leaflet CSS in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 import ClientWrapper from "@/hooks/ClientWrapper";
+import "leaflet/dist/leaflet.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
